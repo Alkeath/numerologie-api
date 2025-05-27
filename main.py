f3556dc31714792071455654551c079f5a72c9b8
Alkeath <alkeath@gmail.com>
2025-05-27 21:20:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from calculs_api import traitement_numerologie_depuis_json  # ✅ importe ta fonction depuis ton fichier
+
+app = FastAPI()
+
+# Pour autoriser les appels du frontend (ex : vercel.app)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Tu pourras restreindre à ton domaine plus tard
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
+
 @app.post("/generer-rapport")
 async def generer_rapport(request: Request):
     data = await request.json()
