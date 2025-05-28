6bcffd7c54bb00b2a3f981fae385129a46cc107a
Alkeath <alkeath@gmail.com>
2025-05-28 13:50:46 +0200

    Update calculs_api.py avec } en moins

--- calculs_api.py
+++ calculs_api.py
@@ -286,7 +286,7 @@ def etape_1_preparer_variables_initiales_et_calculs_avant_test_act(data, lignes)
         "TousPrenoms": normaliser_chaine(data["PrenomNom_TousPrenoms"]).replace(" ", "")
     }
 
-    }
+    
     
     for prefixe in ["UnPrenom", "TousPrenoms"]:
         texte = textes[prefixe]
