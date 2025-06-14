639b62ba844a75403f9e69d6b09a806f7f5520fb
Alkeath <alkeath@gmail.com>
2025-06-14 09:03:56 +0200

    Update main.py

--- services/generationPDF/main.py
+++ services/generationPDF/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uuid
 import os
 from generate_pdf import convert_html_to_pdf
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou une liste précise de domaines autorisés
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class PDFRequest(BaseModel):
     html_url: str  # URL complète du fichier HTML injecté (ex: http://.../fichier.html)
